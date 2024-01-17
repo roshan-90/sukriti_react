c7e149ed4a3f435f67720331231bd6236916b681
roshan-90 <roshan.shriwastav@sukriti.ngo>
2024-01-17 12:49:41 +0530

    pwa is working

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,15 @@ const urlsToCache = [
   "/index.html",
   "/static/css/main.chunk.css",
   "/static/js/bundle.js",
-  "/icon.png",
+  "/assets/icons/icon-48x48.png",
+  "/assets/icons/icon-72x72.png",
+  "/assets/icons/icon-96x96.png",
+  "/assets/icons/icon-128x128.png",
+  "/assets/icons/icon-144x144.png",
+  "/assets/icons/icon-152x152.png",
+  "/assets/icons/icon-192x192.png",
+  "/assets/icons/icon-384x384.png",
+  "/assets/icons/icon-512x512.png",
 ];
 
 self.addEventListener("install", (event) => {
