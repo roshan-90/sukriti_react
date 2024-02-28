521ebf381a9d757ba376a0ebfa0e066e4fe1e414
roshan-90 <roshan.shriwastav@sukriti.ngo>
2024-02-28 17:19:58 +0530

    android enterprise is create component and implement lambda is done

---
+++ src/awsClients/androidEnterpriseLambda.js
@@ -0,0 +1,63 @@
+import AWS from "aws-sdk";
+import Result from "../Entity/Result";
+
+export function executelistEnterprisesAndroidManagementLambda(credentials) {
+  return new Promise(function (resolve, reject) {
+    console.log(
+      "credentials-executelistEnterprisesAndroidManagementLambda",
+      credentials
+    );
+    var lambda = new AWS.Lambda({
+      region: "ap-south-1",
+      apiVersion: "2015-03-31",
+      credentials: credentials, // Pass the credentials from the Redux store
+    });
+    var pullParams = {
+      FunctionName: "List_Enterprises_Android_Management",
+      Payload: "{ " + '"projectId": "' + "ssf-admin-399810" + '"' + "}",
+    };
+
+    lambda.invoke(pullParams, function (err, data) {
+      if (err) {
+        console.log("_lambda", err);
+        reject(err);
+      } else {
+        var pullResults = JSON.parse(data.Payload);
+        console.log("_lambda", pullResults);
+        resolve(pullResults);
+      }
+    });
+  });
+}
+
+export function executelistDevicesAndroidManagementLambda(
+  enterpriseId,
+  credentials
+) {
+  return new Promise(function (resolve, reject) {
+    console.log(
+      "credentials-executelistDevicesAndroidManagementLambda",
+      credentials
+    );
+    var lambda = new AWS.Lambda({
+      region: "ap-south-1",
+      apiVersion: "2015-03-31",
+      credentials: credentials, // Pass the credentials from the Redux store
+    });
+    var pullParams = {
+      FunctionName: "List_Devices_Android_Management",
+      Payload: "{ " + '"enterpriseId": "' + enterpriseId + '"' + "}",
+    };
+
+    lambda.invoke(pullParams, function (err, data) {
+      if (err) {
+        console.log("_lambda", err);
+        reject(err);
+      } else {
+        var pullResults = JSON.parse(data.Payload);
+        console.log("_lambda", pullResults);
+        resolve(pullResults);
+      }
+    });
+  });
+}
--- src/ui/android_management/AndroidDetails.js
+++ src/ui/android_management/AndroidDetails.js
@@ -7,6 +7,7 @@ import React, {
   useCallback,
   useMemo,
 } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ErrorBoundary from "../../components/ErrorBoundary";
 import NoDataComponent from "../../components/NoDataComponent";
 import {
@@ -17,8 +18,86 @@ import {
 } from "../../jsStyles/Style";
 import icToilet from "../../assets/img/icons/ic_toilet.png";
 import { Button } from "reactstrap";
+import "./android.css";
+import {
+  executelistEnterprisesAndroidManagementLambda,
+  executelistDevicesAndroidManagementLambda,
+} from "../../awsClients/androidEnterpriseLambda";
+import { startLoading, stopLoading } from "../../features/loadingSlice";
+import CircularProgress from "@mui/material/CircularProgress";
+import { selectUser } from "../../features/authenticationSlice";
+import { Card, CardBody, CardTitle, CardText, Row, Col } from "reactstrap";
 
 function AndroidDetails() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const isLoading = useSelector((state) => state.loading.isLoading);
+  const [dialogData, setDialogData] = useState(null);
+  const [listEnterprise, setListEnterprise] = useState(undefined);
+  const [listDevices, setListDevices] = useState(undefined);
+  const [showDeviceData, setShowDeviceData] = useState(undefined);
+
+  const handleError = (err, Custommessage, onclick = null) => {
+    console.log("error -->", err);
+    let text = err.message.includes("expired");
+    if (text) {
+      setDialogData({
+        title: "Error",
+        message: err.message,
+        onClickAction: () => {
+          // Handle the action when the user clicks OK
+          console.log(`${Custommessage} -->`, err);
+        },
+      });
+    } else {
+      setDialogData({
+        title: "Error",
+        message: err.message,
+        onClickAction: () => {
+          // Handle the action when the user clicks OK
+          console.log(`${Custommessage} -->`, err);
+        },
+      });
+    }
+  };
+
+  const fetchListEnterprisesData = async () => {
+    try {
+      setListDevices(null);
+      dispatch(startLoading()); // Dispatch the startLoading action
+      var result = await executelistEnterprisesAndroidManagementLambda(
+        user?.credentials
+      );
+      console.log("fetchListEnterprisesData", result);
+      setListEnterprise(result.body.enterprises);
+    } catch (err) {
+      handleError(err, "fetchListEnterprisesData");
+    } finally {
+      dispatch(stopLoading()); // Dispatch the stopLoading action
+    }
+  };
+
+  const fetchListDevicesData = async (enterpriseId) => {
+    try {
+      setListDevices(null);
+      dispatch(startLoading()); // Dispatch the startLoading action
+      var result = await executelistDevicesAndroidManagementLambda(
+        enterpriseId,
+        user?.credentials
+      );
+      console.log("fetchListDevicesData", result);
+      setListDevices(result.body.devices);
+    } catch (err) {
+      handleError(err, "fetchListDevicesData");
+    } finally {
+      dispatch(stopLoading()); // Dispatch the stopLoading action
+    }
+  };
+
+  useEffect(() => {
+    fetchListEnterprisesData();
+  }, []);
+
   const Header = () => {
     return (
       <div
@@ -136,114 +215,216 @@ function AndroidDetails() {
     fontStyle: "normal",
   };
 
+  const handleClickEnterpise = (data) => {
+    fetchListDevicesData(data);
+  };
+
+  const handleClickDevice = (data) => {
+    setShowDeviceData(null);
+    console.log("data", data);
+    setShowDeviceData(data);
+  };
+
   // Rename the function to start with an uppercase letter
-  const DevicesComponent = () => {
-    console.log("hellog");
+  const ListEnterpriseComponent = () => {
+    console.log("listEnterprise", listEnterprise);
     return (
       <div className="row" style={{ background: "white", padding: "5px" }}>
         <Header />
         {/* <NoDataComponent /> */}
-        <div style={{ width: "100%" }}>
-          <div style={{ padding: "10px", overflow: "auto" }}>
-            <div className="row" style={rowStyle}>
-              <div className="col-md-2" style={colStyle}>
-                <div style={circleStyle}></div>
-              </div>
-              <div className="col-md-8" style={textStyle}>
-                Android lists
-              </div>
-            </div>
-            <div className="row" style={rowStyle}>
-              <div className="col-md-2" style={colStyle}>
-                <div style={circleStyle}></div>
-              </div>
-              <div className="col-md-8" style={textStyle}>
-                Android lists2
-              </div>
-            </div>
-            <div className="row" style={rowStyle}>
-              <div className="col-md-2" style={colStyle}>
-                <div style={circleStyle}></div>
-              </div>
-              <div className="col-md-8" style={textStyle}>
-                Android lists3
-              </div>
-            </div>
-            <div className="row" style={rowStyle}>
-              <div className="col-md-2" style={colStyle}>
-                <div style={circleStyle}></div>
-              </div>
-              <div className="col-md-8" style={textStyle}>
-                Android lists4
-              </div>
+        {listEnterprise && (
+          <div style={{ width: "100%" }}>
+            <div style={{ padding: "10px", overflow: "auto" }}>
+              {listEnterprise.map((data, index) => {
+                console.log("chekcing dagta", data);
+                return (
+                  <div className="row" style={rowStyle} key={index}>
+                    <div className="col-md-2" style={colStyle}>
+                      <div style={circleStyle}></div>
+                    </div>
+                    <div
+                      className="col-md-8"
+                      style={textStyle}
+                      onClick={() => handleClickEnterpise(data.name)}
+                    >
+                      {data.name}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
-        </div>
+        )}
       </div>
     );
   };
 
   const memoizedDeviceComponent = useMemo(() => {
-    return <DevicesComponent />;
-  }, []);
+    return <ListEnterpriseComponent />;
+  }, [listEnterprise]);
 
   const ListsDeviceComponent = () => {
-    console.log("hellog");
+    console.log("listDevices", listDevices);
     return (
       <div
         className="row"
         style={{ marginTop: "10px", background: "white", padding: "5px" }}
       >
-        <ListDeviceHeader />
-        <div
-          style={{
-            ...whiteSurface,
-            background: "white",
-            width: "100%",
-            padding: "10px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <div style={circleStyle}></div>{" "}
-          <div style={{ ...complexCompositionStyle.cabinTitle, float: "left" }}>
-            {"child android device"}
-          </div>
-          <Button
-            style={{ float: "right", padding: "0px 0px 0px 0px" }}
-            color="primary"
-            className="px-4"
-            //   onClick={() => setSelectedCabin(props.cabin)}
-          >
-            Details
-          </Button>
-        </div>
+        {listDevices && (
+          <>
+            <ListDeviceHeader />
+            <div
+              style={{
+                ...whiteSurface,
+                background: "white",
+                width: "100%",
+                overflow: "auto",
+                //   display: "flex",
+                //   alignItems: "center",
+                //   justifyContent: "space-between",
+              }}
+            >
+              {listDevices.map((data, index) => {
+                return (
+                  <>
+                    <div className="row" style={rowStyle} key={index}>
+                      <div className="col-md-2" style={colStyle}>
+                        <div style={circleStyle}></div>
+                      </div>
+                      <div
+                        className="col-md-8"
+                        style={textStyle}
+                        onClick={() => handleClickDevice(data)}
+                      >
+                        {data.name}
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </>
+        )}
       </div>
     );
   };
 
   const memoizedListsDeviceComponent = useMemo(() => {
     return <ListsDeviceComponent />;
-  }, []);
+  }, [listDevices]);
 
   const DeviceInfoComponent = () => {
-    console.log("hellog");
+    console.log("showDeviceData", showDeviceData);
     return (
       <>
-        <h2>helo this is</h2>
+        {showDeviceData && (
+          <div className="container">
+            <div className="Qr_image">
+              <img src="assets/img/QRCopyimage.png" alt="QR Code Image" />
+            </div>
+            <div
+              style={{
+                ...whiteSurface,
+                background: "white",
+                width: "100%",
+                overflow: "auto",
+              }}
+            >
+              <Row>
+                <Col md="6">
+                  <Card
+                    style={{
+                      ...whiteSurface,
+                      background: "white",
+                      margin: "10px",
+                    }}
+                  >
+                    <CardBody>
+                      <CardTitle>
+                        <b>Hardware Info</b>
+                      </CardTitle>
+                      <CardText>
+                        <p>Brand: {showDeviceData.hardwareInfo.brand}</p>
+                        <p>Model: {showDeviceData.hardwareInfo.model}</p>
+                        <p>
+                          Serial Number:{" "}
+                          {showDeviceData.hardwareInfo.serialNumber}
+                        </p>
+                        {/* Add more hardware info fields as needed */}
+                      </CardText>
+                    </CardBody>
+                  </Card>
+                  <Card
+                    style={{
+                      ...whiteSurface,
+                      background: "white",
+                      margin: "10px",
+                    }}
+                  >
+                    <CardBody>
+                      <CardTitle>
+                        <b>Software Info</b>
+                      </CardTitle>
+                      <CardText>
+                        <p>
+                          Android Version:{" "}
+                          {showDeviceData.softwareInfo.androidVersion}
+                        </p>
+                        <p>
+                          Build Number:{" "}
+                          {showDeviceData.softwareInfo.androidBuildNumber}
+                        </p>
+                        {/* Add more software info fields as needed */}
+                      </CardText>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col md="6">
+                  <Card
+                    style={{
+                      ...whiteSurface,
+                      background: "white",
+                      margin: "10px",
+                    }}
+                  >
+                    <CardBody>
+                      <CardTitle>
+                        <b>Memory Info</b>
+                      </CardTitle>
+                      <CardText>
+                        <p>Total RAM: {showDeviceData.memoryInfo.totalRam}</p>
+                        <p>
+                          Total Internal Storage:{" "}
+                          {showDeviceData.memoryInfo.totalInternalStorage}
+                        </p>
+                        {/* Add more memory info fields as needed */}
+                      </CardText>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        )}
       </>
     );
   };
 
   const memoizedDeviceInfoComponent = useMemo(() => {
     return <DeviceInfoComponent />;
-  }, []);
+  }, [showDeviceData]);
 
   return (
     <ErrorBoundary>
       <div className="animated fadeIn" style={{ padding: "10px" }}>
-        {/* <Mqtt5 /> */}
+        {isLoading && (
+          <div className="loader-container">
+            <CircularProgress
+              className="loader"
+              style={{ color: "rgb(93 192 166)" }}
+            />
+          </div>
+        )}
         <div className="row">
           <div className="col-md-2" style={{}}>
             {/* <MessageDialog ref={messageDialog} /> */}
