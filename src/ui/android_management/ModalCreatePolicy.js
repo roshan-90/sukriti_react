316581f2057b6ba15df127491f6b0c1bc033969e
roshan-90 <>
2024-06-25 13:26:00 +0530

    application ui is complete

--- src/ui/android_management/ModalCreatePolicy.js
+++ src/ui/android_management/ModalCreatePolicy.js
@@ -10,18 +10,22 @@ import {
 import { Form, FormGroup, Label, Input } from 'reactstrap';
 import { useDispatch, useSelector } from "react-redux";
 import Select from 'react-select'; // Importing react-select
-import { setPolicyName } from "../../features/androidManagementSlice";
 
 const ModalCreatePolicy = ({ data }) => {
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const [title, setTitle] = useState("");
   const [message, setMessage] = useState("");
+  const [policyName, setPolicyName] = useState('');
   const [onClickAction, setOnClickAction] = useState(undefined);
   const [displayName, setDisplayName] = useState(null); // State for react-select
   const listofPolicy = useSelector((state) => state.androidManagement.listOfPolicy);
-  const policyName = useSelector((state) => state.androidManagement.policyName);
   const [selectedDeviceState, setSelectedDeviceState] = useState(null);
+  const [applicationName, setApplicationName] = useState(null);
+  const [installType, setInstallType] = useState(null);
+  const [defaultPermissionPolicy, setDefaultPermissionPolicy] = useState(null);
+  const [autoUpdateMode, setAutoUpdateMode] = useState(null);
+  const [userControlSettings, setUserControlSettings] = useState(null);
   const [formData, setFormData] = useState({
     "cameraDisabled": true,
     "addUserDisabled": true,
@@ -40,18 +44,63 @@ const ModalCreatePolicy = ({ data }) => {
   })
   const [applicationState, setApplicationState] = useState(false);
   const [kioskCustomization , setkioskCustomization] = useState(false)
-  const device_state = [
-    { label: 'DEVICE_STATE_UNSPECIFIED', value: 'DEVICE_STATE_UNSPECIFIED' },
-    { label: 'ACTIVE', value: 'ACTIVE' },
-    { label: 'DISABLED', value: 'DISABLED' },
-    { label: 'DELETED', value: 'DELETED' },
-    { label: 'PROVISIONING', value: 'PROVISIONING' },
-    { label: 'LOST', value: 'LOST' },
-    { label: 'PREPARING_FOR_MIGRATION', value: 'PREPARING_FOR_MIGRATION' },
-    { label: 'DEACTIVATED_BY_DEVICE_FINANCE', value: 'DEACTIVATED_BY_DEVICE_FINANCE' }
+
+  const InstallType = [
+    { label: 'INSTALL_TYPE_UNSPECIFIED', value: 'INSTALL_TYPE_UNSPECIFIED' },
+    { label: 'PREINSTALLED', value: 'PREINSTALLED' },
+    { label: 'FORCE_INSTALLED', value: 'FORCE_INSTALLED' },
+    { label: 'BLOCKED', value: 'BLOCKED' },
+    { label: 'AVAILABLE', value: 'AVAILABLE' },
+    { label: 'REQUIRED_FOR_SETUP', value: 'REQUIRED_FOR_SETUP' },
+    { label: 'KIOSK', value: 'KIOSK' }
+  ];
+ const DefaultPermissionPolicy = [
+    { label: 'PERMISSION_POLICY_UNSPECIFIED', value: 'PERMISSION_POLICY_UNSPECIFIED' },
+    { label: 'PROMPT', value: 'PROMPT' },
+    { label: 'GRANT', value: 'GRANT' },
+    { label: 'DENY', value: 'DENY' }
+  ];
+  const AutoUpdateMode = [
+    { label: 'AUTO_UPDATE_MODE_UNSPECIFIED', value: 'AUTO_UPDATE_MODE_UNSPECIFIED' },
+    { label: 'AUTO_UPDATE_DEFAULT', value: 'AUTO_UPDATE_DEFAULT' },
+    { label: 'AUTO_UPDATE_POSTPONED', value: 'AUTO_UPDATE_POSTPONED' },
+    { label: 'AUTO_UPDATE_HIGH_PRIORITY', value: 'AUTO_UPDATE_HIGH_PRIORITY' }
   ];
 
- 
+  const UserControlSettings = [
+    { label: 'USER_CONTROL_SETTINGS_UNSPECIFIED', value: 'USER_CONTROL_SETTINGS_UNSPECIFIED' },
+    { label: 'USER_CONTROL_ALLOWED', value: 'USER_CONTROL_ALLOWED' },
+    { label: 'USER_CONTROL_DISALLOWED', value: 'USER_CONTROL_DISALLOWED' },
+  ];
+
+ const PowerButtonActions = [
+    { label: 'POWER_BUTTON_ACTIONS_UNSPECIFIED', value: 'POWER_BUTTON_ACTIONS_UNSPECIFIED' },
+    { label: 'POWER_BUTTON_AVAILABLE', value: 'POWER_BUTTON_AVAILABLE' },
+    { label: 'POWER_BUTTON_BLOCKED', value: 'POWER_BUTTON_BLOCKED' },
+  ];
+
+  const systemErrorWarnings = [
+    { label: 'SYSTEM_ERROR_WARNINGS_UNSPECIFIED', value: 'SYSTEM_ERROR_WARNINGS_UNSPECIFIED' },
+    { label: 'ERROR_AND_WARNINGS_ENABLED', value: 'ERROR_AND_WARNINGS_ENABLED' },
+    { label: 'ERROR_AND_WARNINGS_MUTED', value: 'ERROR_AND_WARNINGS_MUTED' }
+  ];
+  const systemNavigation = [
+    { label: 'SYSTEM_NAVIGATION_UNSPECIFIED', value: 'SYSTEM_NAVIGATION_UNSPECIFIED' },
+    { label: 'NAVIGATION_ENABLED', value: 'NAVIGATION_ENABLED' },
+    { label: 'NAVIGATION_DISABLED', value: 'NAVIGATION_DISABLED' },
+    { label: 'HOME_BUTTON_ONLY', value: 'HOME_BUTTON_ONLY' }
+  ];
+  const StatusBar = [
+    { label: 'STATUS_BAR_UNSPECIFIED', value: 'STATUS_BAR_UNSPECIFIED' },
+    { label: 'NOTIFICATIONS_AND_SYSTEM_INFO_ENABLED', value: 'NOTIFICATIONS_AND_SYSTEM_INFO_ENABLED' },
+    { label: 'NOTIFICATIONS_AND_SYSTEM_INFO_DISABLED', value: 'NOTIFICATIONS_AND_SYSTEM_INFO_DISABLED' },
+    { label: 'SYSTEM_INFO_ONLY', value: 'SYSTEM_INFO_ONLY' }
+  ];
+  const DeviceSettings = [
+    { label: 'DEVICE_SETTINGS_UNSPECIFIED', value: 'DEVICE_SETTINGS_UNSPECIFIED' },
+    { label: 'SETTINGS_ACCESS_ALLOWED', value: 'SETTINGS_ACCESS_ALLOWED' },
+    { label: 'SETTINGS_ACCESS_BLOCKED', value: 'SETTINGS_ACCESS_BLOCKED' },
+  ];
 
   useEffect(() => {
     if (data) {
@@ -72,7 +121,6 @@ const ModalCreatePolicy = ({ data }) => {
     if (onClickAction !== undefined) {
       let requestBody = {
         state : selectedDeviceState.value,
-        policyName: policyName.value,
         name: displayName
       }
       onClickAction(requestBody);
@@ -86,8 +134,20 @@ const ModalCreatePolicy = ({ data }) => {
     dispatch(setPolicyName(selectionOption))
   }
 
-  const handleChangeDeviceState = async (selectionOption) => {
-    setSelectedDeviceState(selectionOption);
+  const handleChangeInstallType = async (selectionOption) => {
+    setInstallType(selectionOption);
+  }
+
+  const handleChangeDefaultPermissionPolicy = async (selectionOption) => {
+    setDefaultPermissionPolicy(selectionOption)
+  }
+
+  const handleChangeAutoUpdateMode = async (selectionOption) => {
+    setAutoUpdateMode(selectionOption)
+  }
+
+  const handleChangeUserControlSettings = async (selectionOption) => {
+    setUserControlSettings(selectionOption)
   }
   
   if(data) {
@@ -117,9 +177,23 @@ const ModalCreatePolicy = ({ data }) => {
                 value={message}
               />
             <br/>
+            <Label
+                    check
+                    for="Policy Name"
+                  >
+                    Policy Name
+                </Label>
+              <Input
+                id="policy_name"
+                name="policy_name"
+                placeholder="enter policy_name"
+                type="text"
+                onChange= {(e) => setPolicyName(e.target.value)}
+              />
+            <br/>
             <Form>
             <FormGroup switch>
-              <Label> cameraDisabled </Label>
+              <Label> camera Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.cameraDisabled}
@@ -129,7 +203,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
             </FormGroup>
             <FormGroup switch>
-               <Label> addUserDisabled </Label>
+               <Label> Add User Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.addUserDisabled}
@@ -139,7 +213,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> removeUserDisabled </Label>
+               <Label> Remove User Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.removeUserDisabled}
@@ -149,7 +223,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> factoryResetDisabled </Label>
+               <Label> Factory Reset Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.factoryResetDisabled}
@@ -159,7 +233,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> mountPhysicalMediaDisabled </Label>
+               <Label> Mount Physical Media Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.mountPhysicalMediaDisabled}
@@ -169,7 +243,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> safeBootDisabled </Label>
+               <Label> Safe Boot Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.safeBootDisabled}
@@ -179,7 +253,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> uninstallAppsDisabled </Label>
+               <Label> Uninstall Apps Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.uninstallAppsDisabled}
@@ -189,7 +263,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> bluetoothConfigDisabled </Label>
+               <Label> Bluetooth Config Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.bluetoothConfigDisabled}
@@ -199,7 +273,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> vpnConfigDisabled </Label>
+               <Label> VpnConfig Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.vpnConfigDisabled}
@@ -209,7 +283,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> networkResetDisabled </Label>
+               <Label> Network Reset Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.networkResetDisabled}
@@ -219,7 +293,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> smsDisabled </Label>
+               <Label> Sms Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.smsDisabled}
@@ -229,7 +303,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> modifyAccountsDisabled </Label>
+               <Label> Modify Accounts Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.modifyAccountsDisabled}
@@ -239,7 +313,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> outgoingCallsDisabled </Label>
+               <Label> Outgoing Calls Disabled </Label>
               <Input
                 type="switch"
                 checked={formData.outgoingCallsDisabled}
@@ -249,7 +323,7 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> kioskCustomLauncherEnabled </Label>
+               <Label> Kiosk CustomLauncher Enabled </Label>
               <Input
                 type="switch"
                 checked={formData.kioskCustomLauncherEnabled}
@@ -259,7 +333,17 @@ const ModalCreatePolicy = ({ data }) => {
               />
               </FormGroup>
               <FormGroup switch>
-               <Label> applications </Label>
+               <Label> kioskCustomization </Label>
+              <Input
+                type="switch"
+                checked={kioskCustomization}
+                onClick={() => {
+                  setkioskCustomization(!kioskCustomization);
+                }}
+              />
+              </FormGroup>
+              <FormGroup switch>
+               <Label> Applications </Label>
               <Input
                 type="switch"
                 checked={applicationState}
@@ -271,53 +355,58 @@ const ModalCreatePolicy = ({ data }) => {
               <>
                 {applicationState && (
                   <>
+                        <Label
+                          check
+                          for="Application Name"
+                        >
+                          Application Name
+                      </Label>
+                    <Input
+                      id="application_name"
+                      name="application_name"
+                      placeholder="enter application name"
+                      type="text"
+                      onChange= {(e) => setApplicationName(e.target.value)}
+                    />
+                  <br/>
                     <Select
-                      options={listofPolicy || []}
-                      value={policyName}
-                      onChange={handleChangePolicy}
-                      placeholder="Select Policy"
+                      options={InstallType || []}
+                      value={installType}
+                      onChange={handleChangeInstallType}
+                      placeholder="Select Install Type"
+                      className="select-dropdown"
+                    />
+                    <br/>
+                    <Select
+                      options={DefaultPermissionPolicy || []}
+                      value={defaultPermissionPolicy}
+                      onChange={handleChangeDefaultPermissionPolicy}
+                      placeholder="Select Default Permission Policy"
                       className="select-dropdown"
                     />
                     <br />
                     <Select
-                      options={device_state || []}
-                      value={selectedDeviceState}
-                      onChange={handleChangeDeviceState}
-                      placeholder="Select Device State"
+                      options={AutoUpdateMode || []}
+                      value={autoUpdateMode}
+                      onChange={handleChangeAutoUpdateMode}
+                      placeholder="Select Auto Update Mode"
+                      className="select-dropdown"
+                    />
+                    <br />
+                    <Select
+                      options={UserControlSettings || []}
+                      value={userControlSettings}
+                      onChange={handleChangeUserControlSettings}
+                      placeholder="Select Auto Update Mode"
                       className="select-dropdown"
                     />
                     <br />
                   </>
                 )}
               </>
-              <FormGroup switch>
-               <Label> kioskCustomization </Label>
-              <Input
-                type="switch"
-                checked={kioskCustomization}
-                onClick={() => {
-                  setkioskCustomization(!kioskCustomization);
-                }}
-              />
-              </FormGroup>
             </Form>
             <br/>
-            <Select
-              options={listofPolicy || []}
-              value={policyName}
-              onChange={handleChangePolicy}
-              placeholder="Select Policy"
-              className="select-dropdown"
-            />
-            <br/>
-            <Select
-              options={device_state || []}
-              value={selectedDeviceState}
-              onChange={handleChangeDeviceState}
-              placeholder="Select Device State"
-              className="select-dropdown"
-            />
-            <br/>
+            
           </div>
         </DialogContent>
         <DialogActions>
